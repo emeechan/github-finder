45e4c9d522fd1b729acdd33bd4cbf3381aa3021b
Emily Meechan <emilymeechan1@gmail.com>
2024-04-15 15:29:21 +0100

    adding github api and token

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import UserResults from "../components/layout/users/UserResults"
 
 function Home() {
   return (
-    <div>
-      <h1 className="text-6xl">Welcome</h1>
-    </div>
+    <>
+    {/* search component */}
+      <UserResults />
+      </>
   )
 }
 
