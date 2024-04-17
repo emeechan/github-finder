42dd78ae591bc32446de218ab1418ab710aef1e1
Emily Meechan <emilymeechan1@gmail.com>
2024-04-17 14:35:13 +0100

    cleaning up actions and axios

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -8,22 +8,17 @@ const githubReducer = (state, action) => {
           users: action.payload,
           loading: false,
         }
-        case 'GET_REPOS':
-          return {
-            ...state,
-            repos: action.payload,
-            loading: false,
-          }
         case 'SET_LOADING':
             return {
                 ...state,
                 loading: true,
             }
             //puts the single user in the state
-            case 'GET_USER':
+            case 'GET_USER_AND_REPOS':
               return{
                 ...state,
-                user: action.payload,
+                user: action.payload.user,
+                repos: action.payload.repos,
                 loading: false
               }
             //called from GithubContext
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,7 +5,7 @@ import Spinner from '../components/layout/Spinner'
 import { useParams } from 'react-router-dom'
 import RepoList from '../components/repos/RepoList'
 import GithubContext from '../context/github/GithubContext'
-import { getUser, getUserRepos } from '../context/github/GithubActions'
+import { getUserAndRepos } from '../context/github/GithubActions'
 
 function User() {
 const { user, loading, repos, dispatch} = useContext(GithubContext)
@@ -15,11 +15,9 @@ const params = useParams()
 useEffect(() => {
   dispatch({type: 'SET_LOADING'})
  const getUserData = async() => {
-  const userData = await getUser(params.login)
-  dispatch({type: 'GET_USER', payload: userData})
+  const userData = await getUserAndRepos(params.login)
+  dispatch({type: 'GET_USER_AND_REPOS', payload: userData})
 
-  const userRepoData = await getUserRepos(params.login)
-  dispatch({type: 'GET_REPOS', payload: userRepoData})
  }
 
  getUserData()
